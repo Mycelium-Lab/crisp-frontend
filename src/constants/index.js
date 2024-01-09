a0ab01cb7a139f1fbd7314c4d1f14b9a22650989
sapph1re <dev.romanv@gmail.com>
2024-01-09 15:13:17 -0500

    Updated the contract address

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const CONTRACT_ID = "dev-1704781764767-62803691439443"
+export const CONTRACT_ID = "dev-1704830860080-90505944365613"
 export const METHOD_NAMES = ['get_pools', 'get_balance', 'get_balance_all_tokens', 'open_position', 'close_position', 'swap', 'add_liquidity', 'remove_liquidity', /* 'swap_in', 'swap_out',*/'storage_deposit', 'ft_transfer_call', 'withdraw', 'get_return', 'get_expense', 'create_reserve', 'create_deposit', 'close_deposit', 'refresh_deposits_growth', 'take_deposit_growth', 'get_account_deposits', 'supply_collateral_and_borrow', 'return_collateral_and_repay', 'get_liquidation_list', 'get_borrow_health_factor', 'liquidate', 'positions_opened', 'get_borrows_by_account', 'get_liquidation_price', 'get_max_leverage']
 
 export const DEFAULT_SWAP_PAIR = {
